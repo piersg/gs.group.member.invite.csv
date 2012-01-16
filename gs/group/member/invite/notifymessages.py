9c9641457dad98a2848e77dc33b8dce30d612e36
Michael JasonSmith <mpj17@onlinegroups.net>
2012-01-16 17:00:15 +1100

    Cleaning up the defaults in the messages, and adding a class that can format the admin-message nicely.

--- gs/group/member/invite/notifymessages.py
+++ gs/group/member/invite/notifymessages.py
@@ -5,20 +5,24 @@ from gs.group.base.page import GroupPage
 from gs.profile.email.base.emailuser import EmailUser
 UTF8 = 'utf-8'
 
+def default_message(groupInfo):
+    return u'Please accept this invitation to join %s. I have set '\
+        u'up a profile for you, so you can start participating in '\
+        u'the group as soon as you accept this  invitation.' % \
+        groupInfo.name
+
+def default_subject(groupInfo):
+    return u'Invitation to join %s (Action required)' % groupInfo.name
+
 class InvitationMessage(GroupPage):
     @Lazy
-    def subject(self):
-      retval = u'Invitation to join %s (Action required)' % \
-        self.groupInfo.name
-      return retval
-      
-    @Lazy
     def supportEmail(self):
-        msg = u'Hi!\n\nI received an invitation to join the group '\
-            u'%s\n    %s\nand...' % \
-            (self.groupInfo.name, self.groupInfo.url)
+        sub = (u'Invitation to %s' % self.groupInfo.name).encode(UTF8)
+        msg = (u'Hi!\n\nI received an invitation to join the group '\
+            u'%s\n    %s\nand...' % (self.groupInfo.name, 
+                self.groupInfo.url)).encode(UTF8)
         data = {
-          'Subject':  self.subject.encode(UTF8),
+          'Subject':  sub,
           'body':     msg,
         }
         retval = 'mailto:%s?%s' % \
@@ -31,14 +35,17 @@ class InvitationMessage(GroupPage):
         retval = (a and a[0]) or eu.get_addresses()[0]
         assert retval
         return retval
-        
+    
+    def format_message(self, m):
+        return FormattedMessage(m).html
+    
+    @Lazy
+    def defaultSubject(self):
+        return default_subject(groupInfo)
+    
     @Lazy
     def defaultMessage(self):
-        retval = u'''<p style="margin: 1.385em 0 1.385em 1.385em;">
-        Please accept this invitation to join %s. I
-        have set up a profile for you, so you can start participating
-        in the group as soon as you accept this 
-        invitation.</p>''' % self.groupInfo.name
+        retval = default_message(self.groupInfo)
         return retval.encode(UTF8)
 
     
@@ -51,11 +58,28 @@ class InvitationMessageText(InvitationMessage):
         response.setHeader('Content-Disposition',
                             'inline; filename="%s"' % filename)
 
+    def format_message(self, m):
+        return FormattedMessage(m).txt
+        
+class FormattedMessage(object):
+    def __init__(self, message):
+      self.originalMessage = message
+      
     @Lazy
-    def defaultMessage(self):
-        retval = u'    Please accept this invitation to join %s.\n' \
-            u'    I have set up a profile for you, so you can start '\
-            u'participating\n    in the group as soon as you accept '\
-            u'this invitation.' % self.groupInfo.name
-        return retval.encode(UTF8)
+    def html(self):
+        p = '<p style="margin: 1.385em 0 1.385em 1.385em;">'
+        withPara = self.originalMessage.replace(u'\n\n', u'</p>%s' % p)
+        withBr = withPara.replace('u\n', u'<br/>')
+        retval = '%s%s</p>' % (p, withBr)
+        return retval
+
+    @Lazy
+    def txt(self):
+        tw = TextWrapper(initial_indent = 4, subsequent_indent = 4)
+        retval = ''
+        for line in self.originalMessage.splitlines():
+            p = tw.wrap(line.strip())
+            retval = '%s%s\n\n' % (retval, p)
+        retval = retval.strip()
+        return retval
 
