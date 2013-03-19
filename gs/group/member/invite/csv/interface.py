8b0c413b530685150efb140f2d099f0d5d87894a
Michael JasonSmith <mpj17@onlinegroups.net>
2013-03-20 09:45:14 +1100

    Cleanup of the interface, helped by Ninja IDE
    Former-commit-id: 0410b5f8d2d4a79611a0d89fb3a95f7214e43cb6

--- gs/group/member/invite/csv/interface.py
+++ gs/group/member/invite/csv/interface.py
@@ -1,10 +1,7 @@
 # coding=utf-8
-import re, pytz
-from string import ascii_letters, digits
-from zope.interface.interface import Interface, Invalid, invariant
-from zope.schema import ASCIILine, Bool, Bytes, Choice, List
-from zope.schema import Text, TextLine, URI, ValidationError 
-from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
+from zope.interface.interface import Interface
+from zope.schema import Bytes
+
 
 class ICsv(Interface):
     """ Schema for processing a CSV file. """
@@ -12,4 +9,3 @@ class ICsv(Interface):
             title=u"CSV File",
             description=u'The CSV file to be processed.',
       required=True)
-
