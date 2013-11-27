8482683292e99d43d8e9467b5afd2a5392a335e5
Michael JasonSmith <mpj17@onlinegroups.net>
2013-11-27 15:20:51 -0800

    Adding a Copyright block.
    Former-commit-id: 6093d2cfc334b756cd01e870cbb285e3e9f18b4b

--- setup.py
+++ setup.py
@@ -1,4 +1,18 @@
 # -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright © 2013 OnlineGroups.net and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
 import os
 from setuptools import setup, find_packages
 from version import get_version
@@ -11,15 +25,15 @@ setup(name='gs.group.member.invite.csv',
     long_description=open("README.txt").read() + "\n" +
                     open(os.path.join("docs", "HISTORY.txt")).read(),
     classifiers=[
-      "Development Status :: 4 - Beta",
-      "Environment :: Web Environment",
-      "Framework :: Zope2",
-      "Intended Audience :: Developers",
+        'Development Status :: 5 - Production/Stable',
+        "Environment :: Web Environment",
+        "Framework :: Zope2",
+        "Intended Audience :: Developers",
         'License :: OSI Approved :: Zope Public License',
-      "Natural Language :: English",
-      "Operating System :: POSIX :: Linux"
-      "Programming Language :: Python",
-      "Topic :: Software Development :: Libraries :: Python Modules",
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux"
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     keywords='sign up, registration, profile, user, join, invitation',
     author='Richard Waid',
@@ -42,7 +56,6 @@ setup(name='gs.group.member.invite.csv',
         'Products.GSProfile',
         'Products.XWFCore',
         'Products.CustomUserFolder'
-        # -*- Extra requirements: -*-
     ],
     entry_points="""
     # -*- Entry points: -*-
